aa75b825f69e6b2e6dd8b2d332fc15e3c7924dc9
Arturo Velarde <arturovr82@hotmail.com>
2021-02-13 23:34:20 -0300

    Initial changes

---
+++ SecureClientApp/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+using SampleClientApp.Helper;
+using SampleClientApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace SampleClientApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfidentialClientApplication _app;
+        private readonly IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfidentialClientApplication application, IConfiguration configuration)
+        {
+            _logger = logger;
+            _app = application;
+            _config = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var settings = _config.GetSection("ServiceAd");
+            string[] ResourceIds = new string[] { settings.GetValue<string>("ResourceId") };
+
+            AuthenticationResult result = null;
+            result = await _app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                var httpClient = new HttpClient();
+                var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+
+                if (defaultRequestHeaders.Accept == null ||
+                   !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(new
+                      MediaTypeWithQualityHeaderValue("application/json"));
+                }
+                defaultRequestHeaders.Authorization =
+                  new AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                HttpResponseMessage response = await httpClient.GetAsync(settings.GetValue<string>("BaseAddress"));
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    base.TempData["message"] = json;
+                }
+                else
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    base.TempData["message"] = content;
+                } 
+            }
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SecureClientApp/Helper/SecuritySettings.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleClientApp.Helper
+{
+    public class AuthConfig
+    {
+        public string Instance { get; set; } =
+          "https://login.microsoftonline.com/{0}";
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string Authority
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture,
+                                     Instance, TenantId);
+            }
+        }
+        public string ClientSecret { get; set; }
+        public string BaseAddress { get; set; }
+        public string ResourceID { get; set; }
+
+        public static AuthConfig ReadFromJsonFile(string path)
+        {
+            IConfiguration Configuration;
+
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile(path);
+
+            Configuration = builder.Build();
+
+            var section = Configuration.GetSection("ServiceAd");
+            return section.Get<AuthConfig>();
+        }
+    }
+}
